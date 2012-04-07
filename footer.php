74d6f89e3c76403029162d33558203936be2ff98
TamaraShopsin <tamara@codeforamerica.org>
2012-04-06 21:06:07 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+?>
+
+<hr />
+<div id="footer" role="contentinfo">
+
+	<p class="nospnav">
+		
+	</p>
+</div>
+</div>
+
+
+		<?php wp_footer(); ?>
+</body>
+</html>
